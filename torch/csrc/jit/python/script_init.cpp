a6339c455a13ade3d351d64da46cb45f08c232af
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-10-27 22:31:06 +0200

    [skip ci] Refactor AttributeError to c10

--- torch/csrc/jit/python/script_init.cpp
+++ torch/csrc/jit/python/script_init.cpp
@@ -836,7 +836,7 @@ void initJitScriptBindings(PyObject* module) {
                 try {
                   return toPyObject(self.attr(name));
                 } catch (const ObjectAttributeError& err) {
-                  throw AttributeError("%s", err.what());
+                  C10_THROW_ERROR(AttributeError, err.what());
                 }
               })
           .def(
@@ -857,7 +857,7 @@ void initJitScriptBindings(PyObject* module) {
                   }
                   return toPyObject(self.attr(name));
                 } catch (const ObjectAttributeError& err) {
-                  throw AttributeError("%s", err.what());
+                  C10_THROW_ERROR(AttributeError, err.what());
                 }
               })
           .def(
@@ -887,7 +887,7 @@ void initJitScriptBindings(PyObject* module) {
                   auto ivalue = toIValue(std::move(value), type);
                   self.setattr(name, ivalue);
                 } catch (const ObjectAttributeError& err) {
-                  throw AttributeError("%s", err.what());
+                  C10_THROW_ERROR(AttributeError, err.what());
                 }
               })
           .def(
@@ -1400,8 +1400,9 @@ void initJitScriptBindings(PyObject* module) {
             if (fn) {
               return StrongFunctionPtr(std::move(self), fn);
             } else {
-              throw AttributeError(
-                  "'CompilationUnit' has no attribute '%s'", name.c_str());
+              C10_THROW_ERROR(
+                  AttributeError,
+                  c10::str("'CompilationUnit' has no attribute '", name, "'"));
             }
           })
       .def(
